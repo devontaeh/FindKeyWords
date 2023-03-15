faf39f9722083d053921febed89b4b62480a868e
DeVontae <dhudso3@uwo.ca>
2023-03-15 19:39:49 -0400

    initial commit

--- FindKeyWordsInFile.java
+++ FindKeyWordsInFile.java
@@ -2,9 +2,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class FindKeyWordsInFile {
-        
+
+    public static AVLTree<String, Integer> computeWordFrequencies(String inputFile) {
+
+        AVLTree<String, Integer> wordFrequencies = new AVLTree<>();
+        try (Scanner scan = new Scanner(inputFile)) {
+            while (scan.hasNextLine()) {
+
+                String keyString = scan.nextLine();
+                if (wordFrequencies.get(keyString) != null) {
+                    int frequency = wordFrequencies.get(keyString) + 1;
+                    wordFrequencies.put(keyString, frequency);
+                } else {
+                    wordFrequencies.put(keyString, 1);
+                }
+
+            }
+        }
+
+        return wordFrequencies;
+    }
+
     public static void main(String[] args) {
         if (args.length != 3) {
             System.err.println("Usage: java FindKeyWordsInFile k file.txt MostFrequentEnglishWords.txt");
@@ -12,23 +33,24 @@ public class FindKeyWordsInFile {
         }
 
         int k = Integer.parseInt(args[0]);
-        String inputFileName = args[1];
+        String inputFileFileName = args[1];
         String englishWordsFileName = args[2];
-        
+
         AVLTree<String, Integer> wordFrequencies = new AVLTree<>();
         AVLTree<String, Void> englishWords = new AVLTree<>();
         AVLTree<String, Integer> keywordFrequencies = new AVLTree<>();
-        
+
         try {
-            //Part 1
+
+            // Part 1
             // function name => computeWordFrequencies
-            
-            //Part 2
+
+            // Part 2
             // function name => findKMostFrequentWords
-            
-            //Part 3
+
+            // Part 3
             // function name => filterCommonEnglishWords
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
